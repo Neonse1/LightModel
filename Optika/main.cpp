a5df4cc5426273dc7c6105751fcb5ccd6cd8f948
Neonse <m.danila.alex@gmail.com>
2025-10-23 18:04:57 +0300

    Consts

--- Optika/main.cpp
+++ Optika/main.cpp
@@ -1,1 +1,18 @@
-﻿int main() { return 0; }
+﻿#include <SFML/Graphics.hpp>
+#include <cassert>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <vector>
+
+// ------------------- Константы -------------------
+constexpr double EPS = 1e-12;  // Рассматриваемое приближение
+constexpr double EPS_MOVE =
+    1e-11;  // Сдвиг относительно стены(чтобы луч не начинался в стене)
+constexpr double PI = 3.14159265358979323846;
+constexpr int MAX_REFLECTIONS_DEFAULT = 10000;  // Кап отражений
+constexpr double THRESHOLD_INTENSITY =
+    1.0 / 510.0;  // Минимальная яркость луча, иначе – выход
+
+int main() { return 0; }
